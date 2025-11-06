a4e1912ba25e7909374d129790bdcc1405fd3a66
lautaroadelfino-hub <lautaroadelfino@gmail.com>
2025-11-06 09:28:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import CalculadoraSueldoTandil from './CalculadoraSueldoTandil';
+import CalculadoraSueldoTandil from "./CalculadoraSueldoTandil";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-100">
-      <CalculadoraSueldoTandil />
-    </div>
-  );
+  return <CalculadoraSueldoTandil />;
 }
